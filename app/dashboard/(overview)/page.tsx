3108f732d901a015ed90e6c2d6c0638592687af6
Jose Barros <jose.barros@zeiss.com>
2025-04-21 18:01:55 +0100

    chapter 10

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,3 @@
-import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
@@ -6,8 +5,8 @@ import { Suspense } from 'react';
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardSkeleton } from '@/app/ui/skeletons';
 import { fetchRevenue } from '@/app/lib/data';
 import CardWrapper from '@/app/ui/dashboard/cards';
- 
-export default async function Page() {    
+
+export default async function Page() {
 
     const revenue = await fetchRevenue();
     return (
@@ -21,12 +20,12 @@ export default async function Page() {
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-         <Suspense fallback={<RevenueChartSkeleton />}>
+      <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
-         </Suspense>
-         <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices /> 
-         </Suspense>         
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
     );
--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,11 +4,21 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: Uncomment this code in Chapter 11
 
   // const allPages = generatePagination(currentPage, totalPages);
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const currentPage = Number(searchParams.get('page')) || 1;
+
+  const createPageURL = (page: number | string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set('page', page.toString());
+    return `${pathname}?${params.toString()}`;
+  }
 
   return (
     <>
