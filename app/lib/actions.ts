ddaf2b302f9a7ae6b82a7a1db097f3b0bee6285b
Jose Barros <jose.barros@zeiss.com>
2025-05-02 14:41:30 +0100

    chapter15 fixes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,20 +6,34 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
-import type { Config } from 'tailwindcss';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
-    date: z.string(),
-    });
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
+  date: z.string(),
+});
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
 export async function authenticate(
   prevState: string | undefined,
@@ -35,7 +49,6 @@ export async function authenticate(
       }
     }
     throw error;
-    //return { error: "Error de autenticación 500" };
   }
 }
 
